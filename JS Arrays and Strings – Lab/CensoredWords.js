5c2d2da0dd7d698e26637af7074060e640f86c7d
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-12-12 16:31:58 +0200

    Update CensoredWords.js

--- JS Arrays and Strings – Lab/CensoredWords.js
+++ JS Arrays and Strings – Lab/CensoredWords.js
@@ -1,19 +1,18 @@
 function solve(text,word){
-    let wordToString = String(word)
-    let censored = text.replace(wordToString, repeat(word))
-    while(censored.includes(wordToString)){
-         censored = censored.replace(wordToString, repeat(word))
-    }
-   console.log(censored)
-}
+        let wordToString = String(word)
+        let censored = text.replace(wordToString, repeat(word))
+        while(censored.includes(wordToString)){
+            censored = censored.replace(wordToString, repeat(word))
+        }
+    console.log(censored)
 
-function repeat(word){
-    const wordString = String(word)
-    let array = []
-    for(i=0;i<wordString.length-1;i++){
-        array.push('*')
+    function repeat(word){
+        const wordString = String(word)
+        let array = []
+        for(i=0;i<wordString.length;i++){
+            array.push('*')
+        }
+        return array.join('')
     }
-    return array.join('')
 }
-
 solve('Find the hidden word', 'hidden')
