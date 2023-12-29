408bd8d1c8718662b19e5d039338cea5b1b1cb5a
VasilLozev <108930879+VasilLozev@users.noreply.github.com>
2023-12-29 18:22:30 +0200

    Update solution.js

--- 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
+++ 04. JS-Advanced-DOM-Introduction-Exercise-Resources-New/09. Number Convertor/solution.js
@@ -1,14 +1,27 @@
 function solve() {
-    let option1 = new Option('Hexadecimal', 'hexadecimal')
-    let option2 = new Option('Binary', 'binary')
-    document.querySelector("#selectMenuTo").options.add(option1)
-    document.querySelector("#selectMenuTo").options.add(option2)
-        const btn = document.querySelector('button')
+
+    const newOption = document.createElement('option');
+    const newOption1 = document.createElement('option');
+    const optionText = document.createTextNode('Hexadecimal');
+    const optionText1 = document.createTextNode('Binary');
+    // set option text
+    newOption.appendChild(optionText);
+    newOption1.appendChild(optionText1);
+    // and option value
+    newOption.setAttribute('value','hexadecimal');
+    newOption1.setAttribute('value','binary');
+
+    const select = document.querySelector("#selectMenuTo "); 
+    const select1 = document.querySelector("#selectMenuTo "); 
+    select.appendChild(newOption);
+    select1.appendChild(newOption1);
+
+    const btn = document.querySelector('button')
     btn.onclick = (e) => {
         const selectedValues = [].filter
-            .call(document.querySelector(" #selectMenuTo").options, option => option.selected)
-            .map(option => option.text);
-            if(selectedValues == 'Binary'){
+            .call(document.querySelector("#selectMenuTo").options, option => option.selected)
+            .map(option => option.value);
+            if(selectedValues[0] === 'binary'){
                 let decimal = document.querySelector("#input").value
                 let binary = 0
                 while (decimal > 0) {
@@ -20,10 +33,11 @@ function solve() {
                     decimal = decimal >> 1;
                  }
                 
-                document.querySelector("#result").value = binary
-        } else if (selectedValues == 'Hexadecimal'){
-            const decimal = (Number(document.querySelector("#input").value)).toString(16)
-            document.querySelector("#result").value = decimal.toUpperCase()
+                 document.querySelector("#result").value = binary.split('').slice(0,binary.split('').length-1).join('').toString()
+        }  if(selectedValues[0] === 'hexadecimal'){
+            const decimal = (Number(document.querySelector("#input").value)).toString(16).toUpperCase()
+            document.querySelector("#result").value = decimal
         }
     };
 }
+
